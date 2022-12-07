2232de8a0d0ea5e01e6914fa803d7a3a3bae9311
Omsiva123 <tirupathiraochanda@gmail.com>
2022-12-07 11:45:08 +0530

    initial commit

--- models/Incremental_load/convert_date.sql
+++ models/Incremental_load/convert_date.sql
@@ -1,3 +1,3 @@
-{{ config(post_hook="truncate table {{source('DEVELOPER_DB','US_ACCIDENT_STAGE_INCREMENTAL_TABLE')}}",
+{{ config(post_hook="truncate table {{source('DEVELOPER_DB','ACCIDENT_TABLE_INCREMENTAL')}}",
 materialized='table') }}
-select to_number(to_char(START_TIME, 'yyyymmdd')) as date_id,* from {{source('DEVELOPER_DB','US_ACCIDENT_STAGE_INCREMENTAL_TABLE')}}
+select to_number(to_char(START_TIME, 'yyyymmdd')) as date_id,* from {{source('DEVELOPER_DB','ACCIDENT_TABLE_INCREMENTAL')}}
